5529b4fc48136c131c1f52c0f0b42f67988944cf
shubhamnimje77@gmail.com <shubhamnimje77@gmail.com>
2023-02-17 14:58:43 +0530

    remove hard coded data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './Expenceitem/Expenceitem.css';
 import Expenceitem from './Expenceitem/Expenceitem';
 
 function App() {
--- src/Expenceitem/Expenceitem.js
+++ src/Expenceitem/Expenceitem.js
@@ -1,9 +1,14 @@
 export default function Expenceitem()
 {
+    const expencedate = new Date(2023,2,17);
+    const Expencetitle = 'Panipuri';
+    const Expenceprice = 30;
+    const Expencelocation = 'mumbai';
     return <div>
-        <h1>Expenceitem</h1>
-        <h2>Food Rs 10</h2>
-        <h3>Petrol Rs 100</h3>
-        <h4>Movies Rs 200</h4>
+        <h1>Expence Data</h1>
+        <h2>Date : {expencedate.toISOString()}</h2>
+        <h3>Expence in : {Expencetitle}</h3>
+        <p>Expence Cost : Rs. {Expenceprice}/- Only</p>
+        <p>Expence location{Expencelocation}</p>
     </div>
 }
