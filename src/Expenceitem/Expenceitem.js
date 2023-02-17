c0013412e82d99074dd19c008cf2f1e24cd170a4
shubhamnimje77@gmail.com <shubhamnimje77@gmail.com>
2023-02-17 13:49:09 +0530

    first react code

---
+++ src/Expenceitem/Expenceitem.js
@@ -0,0 +1,9 @@
+export default function Expenceitem()
+{
+    return <div>
+        <h1>Expenceitem</h1>
+        <h2>Food Rs 10</h2>
+        <h3>Petrol Rs 100</h3>
+        <h4>Movies Rs 200</h4>
+    </div>
+}
