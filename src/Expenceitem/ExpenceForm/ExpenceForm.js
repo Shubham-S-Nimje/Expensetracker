17913e1123ec23eaf4df6c0d2759a51e2f61b868
shubhamnimje77@gmail.com <shubhamnimje77@gmail.com>
2023-02-19 19:46:17 +0530

    onChange added to the input

--- src/Expenceitem/ExpenceForm/ExpenceForm.js
+++ src/Expenceitem/ExpenceForm/ExpenceForm.js
@@ -2,16 +2,22 @@ import './ExpenceForm.css';
 import './Expencecard.css';
 import ExpenceCard from './Expencecard'
 const ExpenceForm = () => {
+  const amountchangehandler = (event) => {
+    console.log(event.target.value);
+  };
+  const textchangehandler = (event) => {
+    console.log(event.target.value);
+  };
     return (
     <ExpenceCard className="expenceCard">
       <form className="expenceform">
         <div className="inputtitle">
           <label>Enter Expence Title : </label><br></br>
-          <input type="text" />
+          <input type="text" onChange={textchangehandler}/>
         </div>
         <div className="inputamount">
           <label>Enter Expence Amount : </label><br></br>
-          <input type="text" />
+          <input type="number" onChange={amountchangehandler} />
         </div>
         <div className="inputdate">
           <label>Select Expence Date : </label><br></br>
